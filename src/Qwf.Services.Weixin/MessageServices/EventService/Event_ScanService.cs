e6f1ec617d9fbfdb1f659168338436652d98bbcd
万里QEPC <lileyzhao@gmail.com>
2020-07-28 11:47:21 +0800

    Update Event_ScanService.cs

--- src/Qwf.Services.Weixin/MessageServices/EventService/Event_ScanService.cs
+++ src/Qwf.Services.Weixin/MessageServices/EventService/Event_ScanService.cs
@@ -56,6 +56,26 @@ namespace Qwf.Services.Weixin.MessageServices
                     if (openId.Equals("oAtpFwcxvxtIg0MMRMScGAPUncsA")) CustomApi.SendText(appId, openId, JsonConvert.SerializeObject(ex));
                 }
             }
+            else if (eventKey.Equals("200720HT"))
+            {
+                try
+                {
+                    var (success, data) = await Yiwan.YouzanAPI.UserTags.TagsAdd(openId, "200720海苔[限粉]");
+                    if (success)
+                    {
+                        string gdurl = "https://shop16758627.m.youzan.com/wscgoods/detail/3eo0pgmz97l1n";
+                        CustomApi.SendText(appId, openId, $"购买资格领取成功！\n\n这是<a href=\"{gdurl}\">下单地址</a>\n请尽快支付,商品售完即止则无法支付购买了");
+                    }
+                    else
+                    {
+                        CustomApi.SendText(appId, openId, $"购买资格领取失败！请稍后再试");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (openId.Equals("oAtpFwcxvxtIg0MMRMScGAPUncsA")) CustomApi.SendText(appId, openId, JsonConvert.SerializeObject(ex));
+                }
+            }
         }
     }
 }
